eacd3cd696f6f548317ec462926360457bacf545
PND <fujjisaaan@gmail.com>
2019-05-26 11:36:48 +0900

    refctor fetch_nijisanji_lists.py

--- fetch_nijisanji_lists.py
+++ fetch_nijisanji_lists.py
@@ -1,19 +1,4 @@
-import os
-
-import tweepy
-from dotenv import load_dotenv
-
-
-load_dotenv()
-consumer_key = os.environ.get('CONSUMER_KEY')
-consumer_secret = os.environ.get('CONSUMER_SECRET')
-access_token = os.environ.get('ACCESS_TOKEN')
-access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
+from tweepy_wrapper import api
 
 screen_name = "nijisanji_app"
 for twilist in api.lists_all(screen_name=screen_name):
