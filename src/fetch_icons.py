b0797595a3881b43bf9a933ea8fe44efa0fa4a63
PND <fujjisaaan@gmail.com>
2019-08-30 18:17:22 +0900

    Make `img` directory before saving icon files.

--- src/fetch_icons.py
+++ src/fetch_icons.py
@@ -1,11 +1,14 @@
 import json
 import os
 from urllib.request import urlretrieve
+from pathlib import Path
 
 
 def main(img_dir: str, users_list_path: str):
     with open(users_list_path, mode='r') as f:
         users = json.load(f)
+
+    Path(img_dir).mkdir(exist_ok=True)
 
     for user in users:
         url = user['profile_image_url'].replace('_normal.jpg', '.jpg')
